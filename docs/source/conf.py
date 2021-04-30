c9b2b5b0864af3baab217030d8ea4061e24724c1
Prince2347X <prince.raj29042004@gmail.com>
2021-04-30 15:39:40 +0530

    added theme to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,3 @@
-import re
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -13,6 +12,7 @@ import re
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0,os.path.abspath('.'))
 
@@ -33,7 +33,8 @@ release = "1.0.2"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc"
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -50,8 +51,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+# html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
