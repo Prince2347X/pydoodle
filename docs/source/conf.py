9ee3e75122ae905092cfe360a48a1d2806e8b5d4
Prince Raj <68418241+Prince2347X@users.noreply.github.com>
2021-05-03 00:44:30 +0530

    Fixed imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 import sphinx_rtd_theme
 import sphinx_rtd_dark_mode
+from sphinx.locale import _
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0,os.path.abspath('.'))
 
