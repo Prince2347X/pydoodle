c720ea97034d8f6ba59ecebef03ad7182a241c7c
Prince Raj <68418241+Prince2347X@users.noreply.github.com>
2021-05-03 00:54:49 +0530

    cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 import sphinx_rtd_theme
 import sphinx_rtd_dark_mode
-from sphinx.locale import _
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0,os.path.abspath('.'))
 
@@ -66,30 +65,3 @@ html_static_path = ['_static']
 
 pygments_style = 'friendly'
 default_dark_mode = True
-
-# Extensions to theme docs
-def setup(app):
-    from sphinx.domains.python import PyField
-    from sphinx.util.docfields import Field
-
-    app.add_object_type(
-        'confval',
-        'confval',
-        objname='configuration value',
-        indextemplate='pair: %s; configuration value',
-        doc_field_types=[
-            PyField(
-                'type',
-                label=_('Type'),
-                has_arg=False,
-                names=('type',),
-                bodyrolename='class'
-            ),
-            Field(
-                'default',
-                label=_('Default'),
-                has_arg=False,
-                names=('default',),
-            ),
-        ]
-    )
