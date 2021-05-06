17859ecdc582c8375d1b26cd91149a42337a6796
Prince Raj <68418241+Prince2347X@users.noreply.github.com>
2021-05-06 19:12:50 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@ import sphinx_rtd_theme
 import sphinx_rtd_dark_mode
 
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath("../pydoodle"))
 
 with open("../pydoodle/__init__.py") as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
