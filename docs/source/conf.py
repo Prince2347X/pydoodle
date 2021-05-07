b03a015fbf37ac0d5c0542cfe6515c454b986c7e
Prince Raj <68418241+Prince2347X@users.noreply.github.com>
2021-05-07 06:07:08 +0530

    changes for favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,10 +17,10 @@ import sphinx_rtd_theme
 import sphinx_rtd_dark_mode
 
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath("../pydoodle"))
 
-with open("../pydoodle/__init__.py") as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+version = "v1.1.1"
+
 
 # -- Project information -----------------------------------------------------
 
@@ -57,7 +57,7 @@ exclude_patterns = []
 #
 # html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
-html_favicon = "../docs/source/pydoodle.ico"
+html_favicon = "pydoodle.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
